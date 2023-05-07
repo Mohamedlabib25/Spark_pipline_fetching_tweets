444ac195267035e6c251761ec47228831c69d2cf
MohamedLabib25 <132618266+Mohamedlabib25@users.noreply.github.com>
2023-05-07 11:32:16 +0300

    Update 1_pipline.sh

--- 1_pipline.sh
+++ 1_pipline.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-/opt/spark3/bin/spark-submit --queue streaming 01_continous_listener.py &
-sleep 10
-/opt/spark3/bin/spark-submit --deploy-mode cluster --queue streaming 01_structure_stream.py
 
+# Run the continuous listener in the background in the default queue
+/opt/spark3/bin/spark-submit  01_continous_listener.py &
+
+# Wait for 10 seconds to ensure that the continuous listener is up and running
+sleep 10
+
+# Run the structured streaming job in cluster mode
+/opt/spark3/bin/spark-submit --deploy-mode cluster --queue streaming 01_structure_stream.py
