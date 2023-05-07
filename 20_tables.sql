08600f3424353623ccef0dce65b80bf800038d57
MohamedLabib25 <132618266+Mohamedlabib25@users.noreply.github.com>
2023-05-07 14:40:39 +0300

    Update and rename 20_tables.hql to 20_tables.sql

--- 20_tables.sql
+++ 20_tables.sql
@@ -9,7 +9,7 @@ PARTITIONED BY (year INT, month INT, day INT, hour INT)
 STORED AS PARQUET
 LOCATION '/twitter-landing-data';
 MSCK REPAIR TABLE tweets;
-
+drop table club_raw;
 CREATE EXTERNAL TABLE IF NOT EXISTS club_raw (
   id STRING,
    club STRING,
@@ -23,15 +23,15 @@ SET hive.exec.dynamic.partition.mode=nonstrict;
 INSERT   OVERWRITE TABLE  club_raw PARTITION(year, month, day, hour)
 SELECT DISTINCT  id,
        CASE 
-           WHEN LOWER(word) IN ("real madrid", "realmadrid", "halamadrid", "rmcf", "#halamadrid") THEN 'Real Madrid' 
-           WHEN LOWER(word) IN ("Manchestercity","Manchester City","MCFC","#ManCity") THEN 'Manchester City'
-           WHEN LOWER(word) IN ("intermilan","#ForzaInter","FCIM","Inter Milan") THEN 'Inter Milan'
-           WHEN LOWER(word) IN ("acmilan", "rossoneri", "#forzamilan", "acm") THEN 'AC Milan'
+           WHEN LOWER(word) IN ("real madrid", "realmadrid", "#halamadrid", "rmcf") THEN 'Real Madrid' 
+           WHEN LOWER(word) IN ("manchestercity","manchester city","mcfc","#mancity") THEN 'Manchester City'
+           WHEN LOWER(word) IN (  "intermilan","#forzaInter","fcim","inter milan" ) THEN 'Inter Milan'
+           WHEN LOWER(word) IN ( "ac milan","rossoneri","#forzamilan","acm") THEN 'AC Milan'
            
        END AS club ,tweet_value, year, month, day, hour
 FROM tweets
 LATERAL VIEW explode(words) wordsTable AS word
 WHERE LOWER(word) IN ( "real madrid", "realmadrid", "#halamadrid", "rmcf",
-              "Manchestercity","Manchester City","MCFC","#ManCity",
-               "intermilan","#ForzaInter","FCIM","Inter Milan" ,
-               "AC Milan","Rossoneri","#ForzaMilan","ACM");
+              "manchestercity","manchester city","mcfc","#mancity",
+               "intermilan","#forzaInter","fcim","inter milan" ,
+               "ac milan","rossoneri","#forzamilan","acm");
